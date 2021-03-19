f2165aa186932a43171d0e35110c9f28cf8b5803
Jonathan Garcia <70542639+CoderJonathan@users.noreply.github.com>
2021-03-19 15:25:34 -0700

    MVP Sprint Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from './components/Character';
+import axios from 'axios';
 
 const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +12,30 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+  let id = 0;
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then((response) => {
+      setCharacters(response.data)
+    })
+    .catch(error => {
+      console.log('Whoops!', error);
+    })
+  }, [])
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/225px-Star_wars2.svg.png' 
+        alt="Star Wars logo"></img>
+        <h1 className="Header">Major Characters</h1>
+        {characters.map((character) => (
+          <Character 
+          key={id++} 
+          character={character}
+         />
+        ))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledList = styled.div `
+        
+display: flex;
+flex-direction: column;
+text-shadow: 2px 2px 5px white;
+margin: 1px auto;
+width: 15%;
+
+h2:hover, p:hover {
+transform: scale(1.2);
+transition: all 2s ease-in-out;
+border: black 2px solid;
+cursor: pointer;
+}`
+
+const Character = (prop) => {
+    return(
+        <div>
+        <StyledList>
+            <h2>{prop.character.name}</h2>
+            <p>Height: {prop.character.height}</p>
+            <p>Gender: {prop.character.gender}</p>
+            <p>Mass: {prop.character.mass}</p>
+        </StyledList>   
+        </div>
+    );
+}
+
+export default Character 
